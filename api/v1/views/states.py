cc6d243db4827fb25477599fc42cbf5f5a07dcf0
NuelUzoma <emcool17@gmail.com>
2023-05-02 12:26:04 +0100

    Setup endpoints for the State Class

---
+++ api/v1/views/states.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+Create a new view for State objects that handles all default RESTFul
+API actions: In the file api/v1/views/states.py You must use to_dict()
+to retrieve an object into a valid JSON Update api/v1/views/__init__.py
+to import this new file
+Retrieves the list of all State objects: GET /api/v1/states
+"""
+
+
+from flask import jsonify, request, abort
+from models import storage
+from models.state import State
+from api.v1.views import app_views
+
+
+@app_views.route('/states/', methods=['GET'])
+def all_state():
+    """Retrieves the list of all state objects"""
+    states = storage.all(State).values()
+    list_of_states = [state.to_dict() for state in states]
+    return jsonify(list_of_states)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def single_state(state_id):
+    """Retrieves a state object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    """Deletes a state object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/states/', methods=['POST'])
+def create_state():
+    """Creates a state object"""
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    state = State(**data)
+    storage.new(state)
+    state.save()
+    return jsonify(state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    """Updates a state object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict()), 200
