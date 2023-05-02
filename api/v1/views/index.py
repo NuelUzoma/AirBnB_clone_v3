57d1bb7878c27087e59aca5ae1b6a6d86eba0ae3
NuelUzoma <emcool17@gmail.com>
2023-05-02 09:56:17 +0100

    Resolved Script Typos leading to Server Error

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,6 +11,7 @@ You must use the newly added count() method from storage
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -22,5 +23,12 @@ def status():
 @app_views.route('/api/v1/stats', methods=['GET'])
 def stats():
     """create an endpoint that retrieves the number of each objects by type"""
-    from models import storage
-    storage.count()
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
